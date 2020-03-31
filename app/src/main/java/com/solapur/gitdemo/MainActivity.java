13ac2adf2b4eaada84438b5ca5bae04078dd4862
Gajanan Khare <gajanan.khare@aurea.com>
2020-03-31 16:17:15 +0530

    string test

---
+++ app/src/main/java/com/solapur/gitdemo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.solapur.gitdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    String test="test";
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String Test1;
+    }
+}
